37189936c63cf373219e8d2e4e81c8a7625cca75
RobbePoedts <133092245+Poedtske@users.noreply.github.com>
2024-11-07 00:11:53 +0100

    first initialization with creation seat and ticket entities

---
+++ EA_Project/src/main/java/com/example/ea_project/entities/Ticket.java
@@ -0,0 +1,15 @@
+package com.example.ea_project.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+@Table(name = "tickets")
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
