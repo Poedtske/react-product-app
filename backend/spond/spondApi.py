33257a68b7e979390e55de20f6ba7bf41a8532d0
RobbePoedts <133092245+Poedtske@users.noreply.github.com>
2025-11-30 17:52:19 +0100

    Update spondApi.py
    updated logging

--- backend/spond/spondApi.py
+++ backend/spond/spondApi.py
@@ -9,6 +9,9 @@ from dotenv import load_dotenv
 import os
 import aiohttp  # Import aiohttp for async HTTP requests
 from spond import spond
+import logging
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
+
 
 load_dotenv()
 
@@ -68,7 +71,7 @@ async def get_local_events():
             if response.status == 200:
                 return await response.json()  # Assuming the response contains the events as JSON
             else:
-                print(f"Failed to fetch local events. Status: {response.status}")
+                logging.info(f"Failed to fetch local events. Status: {response.status}")
                 return []
 
 async def delete_event_from_local(event):
@@ -77,10 +80,10 @@ async def delete_event_from_local(event):
         async with session.delete(url, json=event, headers=headers) as response:
             session.close()
             if response.status == 200:
-                print(f"Successfully deleted event")
+                logging.info(f"Successfully deleted event")
                 return True
             else:
-                print(f"Failed to delete event Status: {response.status}")
+                logging.info(f"Failed to delete event Status: {response.status}")
                 return False
 
 async def post_event_to_local(event):
@@ -89,10 +92,10 @@ async def post_event_to_local(event):
         async with session.post(url, json=event, headers=headers) as response:
             session.close()
             if response.status == 200:
-                print(f"Successfully added event with ID {event['spondId']}")
+                logging.info(f"Successfully added event with ID {event['spondId']}")
                 return True
             else:
-                print(f"Failed to add event with ID {event['spondId']}. Status: {response.status}")
+                logging.info(f"Failed to add event with ID {event['spondId']}. Status: {response.status}")
                 return False
 
 async def update_event_in_local(event):
@@ -101,10 +104,10 @@ async def update_event_in_local(event):
         async with session.put(url, json=event, headers=headers) as response:
             session.close()
             if response.status == 200:
-                print(f"Successfully updated event with ID {event['spondId']}")    
+                logging.info(f"Successfully updated event with ID {event['spondId']}")    
                 return True           
             else:
-                print(f"Failed to update event with ID {event['spondId']}. Status: {response.status}")
+                logging.info(f"Failed to update event with ID {event['spondId']}. Status: {response.status}")
                 return False
 
 
@@ -115,7 +118,7 @@ async def compare_and_update_events(spond_events, local_events):
     for local_event in local_events:
         if 'spondId' in local_event:
             if local_event['spondId'] not in {event['spondId'] for event in spond_events}:
-                print(f"Event {local_event['spondId']} is no longer in Spond, deleting locally.")                
+                logging.info(f"Event {local_event['spondId']} is no longer in Spond, deleting locally.")                
                 boolean = await delete_event_from_local(local_event)
                 if boolean:
                     local_events.remove(local_event)
@@ -125,22 +128,22 @@ async def compare_and_update_events(spond_events, local_events):
         local_event = local_events_map.get(spond_event['spondId'])
 
         if not local_event:
-            print(f"Event {spond_event['spondId']} does not exist locally, adding it.")
+            logging.info(f"Event {spond_event['spondId']} does not exist locally, adding it.")
             boolean = await post_event_to_local(spond_event)
             if boolean:
                 local_events.append(spond_event)
         elif local_event != spond_event:
-            print(f"Event {spond_event['spondId']} differs locally, updating it.")
-            print(local_event)
-            print("----------------------------------------------")
-            print(spond_event)
+            logging.info(f"Event {spond_event['spondId']} differs locally, updating it.")
+            logging.info(local_event)
+            logging.info("----------------------------------------------")
+            logging.info(spond_event)
             boolean = await update_event_in_local(spond_event)
             if boolean:
                 local_event = spond_event
 
 async def main():
     sleep(60)
-    print("Fetching Spond events and comparing with local events...")
+    logging.info("Fetching Spond events and comparing with local events...")
     local_events = []
     firsttime = True
     while True:
@@ -155,11 +158,11 @@ async def main():
                 await compare_and_update_events(spond_events, local_events)
 
         except Exception as e:
-            print(f"Error occurred: {e}")
+            logging.info(f"Error occurred: {e}")
             sys.exit(1)  # Exit the script and stop the container
 
         await asyncio.sleep(5)  # Add a small sleep to prevent tight loops
-        print("Done. Sleeping for 24 hours.")
+        logging.info("Done. Sleeping for 24 hours.")
         sleep(24 * 60 * 60)  # Sleep for 24 hours
 
 if __name__ == "__main__":
